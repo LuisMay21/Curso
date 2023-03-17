0dd3dd53d8cc374544882f168020d10ccaeaac22
LuisMay21 <123luismj@gmail.com>
2023-03-17 14:05:24 -0600

    Segundo

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+ public static void main(String [] sis)
+ {
+double a=14,b=20,c;
+
+c=a*b;
+
+System.out.println("El area del rectangulo de lados "+a+" y "+b+" es "+c);
+ }    
+
+}
---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String []sys)
+    {
+        int a=0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingresa Numero");
+        a=scan.nextInt();
+
+        
+
+    }
+    
+}
